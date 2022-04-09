0daa2e65c0c4c7ee1e6cf48884a062975a8bab10
Tim Crisp <timothycrisp@gmail.com>
2022-04-08 20:50:30 -0500

    update a toy

--- src/services/toys.js
+++ src/services/toys.js
@@ -25,3 +25,28 @@ export const getAll = async () => {
     throw error
   }
 }
+
+export const getOne = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}${id}`)
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
+
+export const update = async (toy) => {
+  try {
+    const res = await fetch(`${BASE_URL}${toy.id}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(toy)
+    })
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
